5b26643a6e9c28340e0cce99d391236078d6c29d
SametYILMAZ <sametyilmazx@gmail.com>
2020-01-26 15:03:24 +0300

    [DEV] PhpMailer service entegration.

--- index.php
+++ index.php
@@ -1,33 +1,57 @@
 <?php
 
-//https://www.yoncu.com/apiler/domain/get/sorgula.php?aa=domainadi.com
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
 
-function sorgula(){
-  $curl = curl_init();
+require 'vendor/autoload.php';
 
-  curl_setopt($curl, CURLOPT_URL, 'https://www.yoncu.com/apiler/domain/get/sorgula.php');
-  curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
-  curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
-  curl_setopt($curl, CURLOPT_POST, 1);
-  curl_setopt($curl, CURLOPT_POSTFIELDS, "aa=domainadi.com");
+$domainName = 'facebook.com'; // Domain name
+$emailAddress = 'sametyilmazx@gmail.com'; // Gmail address
+$mailPassword = 'xxxs123xxx..'; // Gmail password
 
-  $result = curl_exec($curl);
+function domainControl($domainName)
+{
+    $apiAddress = 'https://www.yoncu.com/apiler/domain/get/sorgula.php';
 
-  curl_close($curl);
+    $curl = curl_init();
 
-  return $result;
-}
-$donendeger = array();
-$donendeger = json_decode(sorgula(),true);
+    curl_setopt($curl, CURLOPT_URL, $apiAddress);
+    curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($curl, CURLOPT_POST, 1);
+    curl_setopt($curl, CURLOPT_POSTFIELDS, 'aa=' . $domainName);
 
-$kontrol = $donendeger[1]['domainadi.com'];
+    $result = curl_exec($curl);
+    curl_close($curl);
 
-// echo $kontrol;
-
-if($kontrol!="DOLU" || $kontrol=="BOS" || $kontrol=="BOŞ"){
-
-  require("mesajgonder/send.php");
-
+    // Exp $result = '[true,{"facebook.com":"DOLU"}]';
+    return $result;
 }
 
-?>
+$domainStatus = json_decode(domainControl($domainName), true);
+$domainStatusResult = $domainStatus[1][$domainName];
+
+if ($domainStatusResult !== 'DOLU' || $domainStatusResult === 'BOS' || $domainStatusResult === 'BOŞ') {
+
+    $mail = new PHPMailer(true);
+
+    $mail->IsSMTP();
+    $mail->Host = 'smtp.gmail.com';
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+    $mail->SMTPAuth = true;
+    $mail->Port = 587;
+    $mail->SMTPSecure = 'tls';
+    $mail->Username = $emailAddress;
+    $mail->Password = $mailPassword;
+
+    try {
+        $mail->setFrom($emailAddress, 'Domain Control Service');
+        $mail->addAddress($emailAddress, 'Domain Control Service');
+        $mail->CharSet = 'UTF-8';
+        $mail->Subject = 'Domain Control Service';
+        $mail->msgHTML('Heyyy!!! ' . $domainName . ' is purchasable!!');
+        $mail->Send();
+    } catch (Exception $e) {
+        echo 'Error: ' . $e->getMessage();
+    }
+}
